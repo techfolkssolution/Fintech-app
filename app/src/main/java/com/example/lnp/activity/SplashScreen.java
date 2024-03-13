b37d1c7c6852eeda74b4651deb7f823bceea51e2
Jatindarji03 <djatin292@gmail.com>
2024-03-13 18:30:44 +0530

    Login Activity

---
+++ app/src/main/java/com/example/lnp/activity/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.lnp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.lnp.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                sendToLoginActivity();
+            }
+        },2500);
+
+    }
+    public void sendToLoginActivity(){
+        Intent intent=new Intent(SplashScreen.this,Login.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+}
