50c78e2a9abdb2628c80ae57033ed95fac1aaa94
Jatindarji03 <djatin292@gmail.com>
2024-03-19 17:51:13 +0530

    UserInformation Activity

---
+++ app/src/main/java/com/example/lnp/activity/UserInformation.java
@@ -0,0 +1,16 @@
+package com.example.lnp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lnp.R;
+
+public class UserInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_information);
+    }
+}
